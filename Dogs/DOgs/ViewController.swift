15ede4a7814b6b7c932f29c7e63ea3bdf05904b7
Vitalii Kuznetsov <v-blacksmith@hotmail.com>
2022-04-17 12:01:08 +0200

    Setup base project

---
+++ Dogs/DOgs/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Dogs
+//
+//  Created by vitalii.kuznetsov on 2022-04-17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
