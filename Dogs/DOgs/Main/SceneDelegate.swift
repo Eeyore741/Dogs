3ca10dfef3069102a6870b72dcb81709f12b026f
Vitalii Kuznetsov <v-blacksmith@hotmail.com>
2022-07-09 18:10:00 +0200

    Remove redundant var

--- Dogs/DOgs/Main/SceneDelegate.swift
+++ Dogs/DOgs/Main/SceneDelegate.swift
@@ -11,7 +11,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
     
-    private var applicationController: ApplicationController = ApplicationController()
+    private let applicationController: ApplicationController = ApplicationController()
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let scene = (scene as? UIWindowScene) else { return }
